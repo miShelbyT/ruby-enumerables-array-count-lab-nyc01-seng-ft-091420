f8646cf5797371f51d1601711dd2ac8fbb9a8ecd
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-28 15:10:35 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -9,5 +9,6 @@ end
 def count_empty_strings(array)
   array.count do |word|
     word == " "
+  end
 end
   # Return the total number of EMPTY strings in the provided array using the count enumerable
